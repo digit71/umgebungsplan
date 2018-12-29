8d98ab2ade9c64193cceb81bd7c9db370c267733
ddeeken <ddeeken@dirks-mbp.fritz.box>
2018-12-29 01:29:13 +0100

    initial commit

---
+++ src/test/java/com/pixotec/umgebungsplan/UmgebungsplanApplicationTests.java
@@ -0,0 +1,17 @@
+package com.pixotec.umgebungsplan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UmgebungsplanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
